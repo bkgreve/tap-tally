11a0218319af0a9b63aeba95fb58579524ec1c39
Ben Greve <bkgreve@gmail.com>
2020-09-18 04:51:05 -0400

    Control center

--- tap_tally/__init__.py
+++ tap_tally/__init__.py
@@ -1,15 +1,21 @@
 import os
 
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app(test_config=None):
     """Flask Application Factory"""
     app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
+
     app.config.from_mapping(
         SECRET_KEY=os.getenv('FLASK_SECRET_KEY', 'changeme'),
         DATABASE=os.path.join(app.instance_path, 'tap-tally.sqlite'),
     )
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    db = SQLAlchemy(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
--- tap_tally/control_center/views.py
+++ tap_tally/control_center/views.py
@@ -1,6 +1,24 @@
+import logging
+
+from flask import render_template, request, redirect, url_for
+
 from . import control_center
+from .forms import TapEntryForm
 
 
-@control_center.route('/')
-def hello():
-    return "Hello"
+@control_center.route('/', methods=['POST', 'GET'])
+def control_dashboard():
+    """Control center dashboard view"""
+    return render_template('dashboard.html')
+
+
+@control_center.route('/new-entry', methods=['POST', 'GET'])
+def new_entry():
+    """Creates new entry"""
+    form = TapEntryForm()
+    if request.method == 'POST' and form.validate():
+        return redirect(url_for('control_center.new_entry'))
+    return render_template('new_entry.html',
+                           title='Control Center',
+                           form=form,
+                           )
