3718b6260e49133e594e2fbf9acebea7620ee14f
Ben Greve <bkgreve@gmail.com>
2020-09-21 07:16:33 -0400

    Setup sqlite

--- tap_tally/__init__.py
+++ tap_tally/__init__.py
@@ -4,18 +4,24 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
 def create_app(test_config=None):
     """Flask Application Factory"""
-    app = Flask(__name__, instance_relative_config=True)
-    Bootstrap(app)
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+        static_url_path='/flask_static/',
+        static_folder='flask_static/'
+    )
 
     app.config.from_mapping(
         SECRET_KEY=os.getenv('FLASK_SECRET_KEY', 'changeme'),
-        DATABASE=os.path.join(app.instance_path, 'tap-tally.sqlite'),
+        SQLALCHEMY_DATABASE_URI=os.getenv(
+            'SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/tap-tally.db'
+        ),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-    db = SQLAlchemy(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -27,6 +33,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    db.init_app(app)
+    Bootstrap(app)
+
     # Register blueprints for different modules
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
@@ -35,5 +44,5 @@ def create_app(test_config=None):
     app.register_blueprint(
         control_blueprint, url_prefix='/control-center'
     )
-
+    db.create_all(app=app)
     return app
