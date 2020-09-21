16e5e435bb856df8f3f3c3e29d7fe1e90684f940
Ben Greve <bkgreve@gmail.com>
2020-09-21 07:17:53 -0400

    Add and edit entry endpoints

--- tap_tally/control_center/views.py
+++ tap_tally/control_center/views.py
@@ -1,24 +1,57 @@
 import logging
 
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 
 from . import control_center
-from .forms import TapEntryForm
+from .forms import TapEntryForm, SelectEntries
+from ..models.tap_entry import TapEntry
+from .. import db
 
 
-@control_center.route('/', methods=['POST', 'GET'])
+@control_center.route('/', methods=['GET'])
 def control_dashboard():
     """Control center dashboard view"""
-    return render_template('dashboard.html')
+    visible_entries = (db.session.query(TapEntry)
+                       .filter(TapEntry.visible == True)
+                       .all()
+                       )
+    hidden_entries = (TapEntry.query.filter(TapEntry.visible == False))
+
+    return render_template('dashboard.html',
+                           visible_entries=visible_entries,
+                           hidden_entries=hidden_entries,
+                           )
 
 
 @control_center.route('/new-entry', methods=['POST', 'GET'])
 def new_entry():
     """Creates new entry"""
     form = TapEntryForm()
+    entry = TapEntry()
     if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        db.session.add(entry)
+        db.session.commit()
+        flash(f"{form.name.data} added.")
         return redirect(url_for('control_center.new_entry'))
-    return render_template('new_entry.html',
-                           title='Control Center',
+    return render_template('entry.html',
+                           title='Add New Entry',
+                           form=form,
+                           )
+
+
+@control_center.route('/edit-entry/<entry_id>', methods=['POST', 'GET'])
+def edit_entry(entry_id):
+    if not entry_id:
+        return redirect(url_for('control_center.new_entry'))
+    entry = db.session.query(TapEntry).get(entry_id)
+    form = TapEntryForm(obj=entry)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        db.session.add(entry)
+        db.session.commit()
+        return redirect(url_for('control_center.control_dashboard'))
+    return render_template('entry.html',
+                           title='View/Edit Entry',
                            form=form,
                            )
