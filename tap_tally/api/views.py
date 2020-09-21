2b0692a0a1277e1ff796dc88c38223671c1d291b
Ben Greve <bkgreve@gmail.com>
2020-09-21 07:17:06 -0400

    Retrieve data from DB instead of JSON file

--- tap_tally/api/views.py
+++ tap_tally/api/views.py
@@ -1,23 +1,35 @@
 import os
 import json
+import logging
 
 from . import api
+from ..models.tap_entry import TapEntry
+from .. import db
 
 
 @api.route('/entries')
 def get_tap_entries():
     """API Endpoint for returning Tap Entries to display"""
-    with open('/usr/src/app/data/beers.json') as f:
-        beer_data = json.load(f)
+    tap_entires = TapEntry.query.filter(TapEntry.visible == True)
     entries = []
-    for entry in beer_data:
-        if entry.get('visible', False):
-            image_name = entry.get('image', False)
-            if image_name:
-                image_exists = os.path.exists(f"/usr/src/app/data/images/{image_name}")
-                if not image_exists:
-                    entry['image'] = None
-            entries.append(entry)
+    for entry in tap_entires:
+        brewed = entry.brewed_on.strftime('%b. %d, %Y')
+        kegged = None
+        if entry.kegged_on:
+            kegged = entry.kegged_on.strftime('%b. %d, %Y')
+        display_entry = {
+            "tapNo": entry.tap_number,
+            "alcByVol": entry.alc_by_vol,
+            "beerName": entry.name,
+            "beerSRM": entry.srm,
+            "beerIBU": entry.ibu,
+            "brewedOn": brewed,
+            "keggedOn": kegged,
+            "kegNo": entry.keg_no,
+            "beerDescription": entry.description,
+            "image": False
+        }
+        entries.append(display_entry)
     return {
         'entries': entries
     }
